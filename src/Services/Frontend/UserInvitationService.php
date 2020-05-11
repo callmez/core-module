74c2d4e5eb07c786ff9ef0c5b282b48c8c3c68c8
龙张 <callme-z@qq.com>
2020-05-11 15:20:45 +0800

    no message

--- src/Services/Frontend/UserInvitationService.php
+++ src/Services/Frontend/UserInvitationService.php
@@ -2,17 +2,17 @@
 
 namespace Modules\Core\Services\Frontend;
 
-use Closure;
-use UnexpectedValueException;
 use App\Models\User;
 use Carbon\Carbon;
+use Closure;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Modules\Core\Exceptions\ModelSaveException;
-use Modules\Core\Services\Traits\HasQuery;
 use Modules\Core\Events\Frontend\UserInvited;
+use Modules\Core\Exceptions\ModelSaveException;
 use Modules\Core\Models\Frontend\UserInvitation;
+use Modules\Core\Services\Traits\HasQuery;
+use UnexpectedValueException;
 
 class UserInvitationService
 {
@@ -32,18 +32,60 @@ class UserInvitationService
     }
 
     /**
-     * @param \Closure|array|null $where
+     * @param $user
+     * @param null $token
+     * @param null $expiredAt
      * @param array $options
      *
-     * @return mixed
+     * @return bool|UserInvitation
      */
-    public function one($where = null, array $options = [])
+    public function createWithUser($user, $token = null, $expiredAt = null, array $options = [])
     {
-        return $this->queryOne($where, array_merge([
-            'exception' => function() {
-                return new ModelNotFoundException(trans('邀请码未找到'));
+        return $this->create([
+            'user_id'    => with_user_id($user),
+            'token'      => $token,
+            'expired_at' => $expiredAt,
+        ], $options);
+    }
+
+    /**
+     * @param array $data
+     * @param array $options
+     *
+     * @return bool|UserInvitation
+     */
+    public function create(array $data, array $options = [])
+    {
+        return $this->queryCreate(array_merge($data, [
+            'token'      => $data['token'] ?: $this->generateUniqueToken(),
+            'expired_at' => $data['expired_at'] ?: Carbon::now()->addSeconds(config('core::user.invitation.expires',
+                600)),
+        ]), $options);
+    }
+
+    /**
+     * @param Closure|null $tokenCallback
+     * @param array $otpions
+     *
+     * @return mixed|string
+     * @throws ModelNotFoundException
+     */
+    public function generateUniqueToken(Closure $tokenCallback = null, array $options = [])
+    {
+        $i = 1;
+        $max = $options['max'] ?? 10;
+        while (true) {
+            $token = is_callable($tokenCallback) ? $tokenCallback() : Str::random(6);
+            $invitation = $this->getByToken($token, ['exception' => false]);
+
+            if ( ! $invitation) {
+                return $token;
+            } elseif ($i > $max) {
+                throw new UnexpectedValueException('Max generate user invitation token times.');
             }
-        ], $options));
+
+            $i++;
+        }
     }
 
     /**
@@ -76,59 +118,18 @@ class UserInvitationService
     }
 
     /**
-     * @param Closure|null $tokenCallback
-     * @param array $otpions
-     *
-     * @return mixed|string
-     * @throws ModelNotFoundException
-     */
-    public function generateUniqueToken(Closure $tokenCallback = null, array $options = [])
-    {
-        $i = 1;
-        $max = $options['max'] ?? 10;
-        while (true) {
-            $token = is_callable($tokenCallback) ? $tokenCallback() : Str::random(6);
-            $invitation = $this->getByToken($token, ['exception' => false]);
-
-            if ( ! $invitation) {
-                return $token;
-            } elseif ($i > $max) {
-                throw new UnexpectedValueException('Max generate user invitation token times.');
-            }
-
-            $i++;
-        }
-    }
-
-    /**
-     * @param $user
-     * @param null $token
-     * @param null $expiredAt
+     * @param \Closure|array|null $where
      * @param array $options
      *
-     * @return bool|UserInvitation
+     * @return mixed
      */
-    public function createWithUser($user, $token = null, $expiredAt = null, array $options = [])
+    public function one($where = null, array $options = [])
     {
-        return $this->create([
-            'user_id' => with_user_id($user),
-            'token' => $token === null ? $this->generateUniqueToken() : $token,
-            'expired_at' => $expiredAt
-        ], $options);
-    }
-
-    /**
-     * @param array $data
-     * @param array $options
-     *
-     * @return bool|UserInvitation
-     */
-    public function create(array $data, array $options = [])
-    {
-        return $this->queryCreate(array_merge($data, [
-            'token'      => $data['token'] ?: $this->generateUniqueToken(),
-            'expired_at' => $data['expired_at'] ?: Carbon::now()->addSeconds(config('core::user.invitation.expires', 600)),
-        ]), $options);
+        return $this->queryOne($where, array_merge([
+            'exception' => function () {
+                return new ModelNotFoundException(trans('邀请码未找到'));
+            },
+        ], $options));
     }
 
     /**
@@ -154,10 +155,11 @@ class UserInvitationService
         /** @var UserService $userService */
         $userService = resolve(UserService::class);
 
-        return $userService->all(function($query) use ($data) {
+        return $userService->all(function ($query) use ($data) {
             $query->whereIn('id', $data);
         }, $options['allOptions'] ?? [])->keyBy('id');
     }
+
 
     /**
      * @param array $data
@@ -170,7 +172,7 @@ class UserInvitationService
         $invitationState = $options['invitation'] ?? 1;
 
         if ($invitationState == 0) { // 不开启邀请码
-            return ;
+            return;
         }
 
         if ($invitationState == 1) { // 一码一人模式
@@ -198,7 +200,7 @@ class UserInvitationService
 
         $invitation->setUsed($usedUser);
 
-        if (!$invitation->save()) {
+        if ( ! $invitation->save()) {
             throw ModelSaveException::withModel($invitation);
         }
 
@@ -222,7 +224,7 @@ class UserInvitationService
         $usedInvitation = $invitation->replicate();
         $usedInvitation->setUsed($usedUser);
 
-        if (!$usedInvitation->save()) {
+        if ( ! $usedInvitation->save()) {
             throw ModelSaveException::withModel($usedInvitation);
         }
 
