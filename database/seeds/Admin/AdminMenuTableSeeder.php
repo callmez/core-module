79994a7ae3153da220c3bffdbf96a7ab13375c28
aoxiang <makenmail@qq.com>
2020-05-14 17:35:46 +0800

    fix bug

--- database/seeds/Admin/AdminMenuTableSeeder.php
+++ database/seeds/Admin/AdminMenuTableSeeder.php
@@ -18,35 +18,35 @@ class AdminMenuTableSeeder extends Seeder
             'title' => '用户管理',
             'icon' => 'layui-icon-user',
             'url' => '',
-            'is_show' => 1,
+            'status' => 1,
         ]);
 
         $system = AdminMenu::create([
             'title' => '系统管理',
             'icon' => 'icon-set',
             'url' => '',
-            'is_show' => 1,
+            'status' => 1,
         ]);
 
         $role = AdminMenu::create([
             'title' => '角色权限',
             'parent_id' => $system->id,
             'url' => route('admin.auth.roles', [], false),
-            'is_show' => 1,
+            'status' => 1,
         ]);
 
         $module = AdminMenu::create([
             'title' => '模块管理',
             'parent_id' => $system->id,
             'url' => route('admin.module.modules', [], false),
-            'is_show' => 1,
+            'status' => 1,
         ]);
 
 //        $queue = AdminMenu::create([
 //            'title' => '队列监控',
 //            'parent_id' => $system->id,
 //            'url' => route('horizon.index', ['view' => 'dashboard'], false),
-//            'is_show' => 1,
+//            'status' => 1,
 //        ]);
     }
 }
